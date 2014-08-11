dbd1c20f255e00aaf5ecdbcd080d6df3c7710565
Stephen Woodbridge <woodbri@swoodbridge.com>
2014-08-11 14:37:13 -0400

    Update twnode and start on path.h

---
+++ new/path.h
@@ -0,0 +1,18 @@
+#ifndef PATH_H
+#define PATH_H
+
+template<class T>
+class Path {
+  private:
+    T home;
+    std::deque<T> path;
+
+  public:
+    // TODO: how can we leverage the existing deque methods
+
+
+};
+
+
+#endif
+
--- new/twnode.h
+++ new/twnode.h
@@ -15,15 +15,15 @@ class Twnode: public Node {
   public:
     int opens() const {return tw_open;};
     int closes() const {return tw_close;};
-    int getDemand() const{ return demand;};
-    int getServiceTime() const{  return service;};
-    int windowLength() const { return  tw_close - tw_open; };
+    int getdemand() const{ return demand;};
+    int getservicetime() const{  return service;};
+    int windowlength() const { return  tw_close - tw_open; };
 
-    bool checkIntegrity() const;
+    bool checkintegrity() const;
     bool ispickup() const { return getdemand()>0; };
     bool isdelivery() const { return getdemand()<0; };
-    bool earlyarrival(double D) const { return D < tw_open; };
-    bool latearrival(double D) const { return D > tw_close; };
+    bool earlyarrival(const double D) const { return D < tw_open; };
+    bool latearrival(const double D) const { return D > tw_close; };
 
     void dump() const;
 
@@ -32,7 +32,7 @@ class Twnode: public Node {
     void setdemand(int v) { demand = v; };
     void settwopen(int v) { tw_open = v; };
     void settwclose(int v) { tw_close = v; };
-    voide setservice(int v) { service = v; };
+    void setservice(int v) { service = v; };
 
     Twnode() {
         Node();
