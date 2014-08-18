08785961e212c815f18a2a7b7b97ad009764f2eb
mapas de vicky <vicky_vergara@hotmail.com>
2014-08-18 14:07:37 -0500

    making the new version as twpath1

--- baseClasses/twpath1.h
+++ baseClasses/twpath1.h
@@ -15,22 +15,24 @@
     * what should prev() and next() do at the begin(0 and end() ?
 */
 
-template <class knode> class Twpath {
+template <class Knode> class Twpath {
   protected:
-    knode home;
-    knode dumpsite;
-    std::deque<knode> path;
+    Knode home;
+    Knode dumpsite;
+    std::deque<Knode> path;
 
   public:
    /* node related */
    double getnid(int i) const  {return path[i].getnid();}; 
    double getx(int i) const  {return path[i].getx();}; 
    double gety(int i) const  {return path[i].gety();}; 
+   double distance(int i, int j) {return path[i].distance(path[j]);}
+   double distance(int i, Knode n) {return path[i].distance(n);}
    bool isvalid(int i) const { return path[i].isvalid(); };
    bool issamepos(int i,int j) const { return path[i].isSamePos(path[j]); };
    bool issamepos(int i,int j,double tol) const { return path[i].isSamePos(path[j],tol); };
-   bool issamepos(int i,const Node &n) const { return path[i].isSamePos(n); };
-   bool issamepos(int i,const Node &n, double tol) const { return path[i].isSamePos(n,tol); };
+   bool issamepos(int i,const Knode &n) const { return path[i].isSamePos(n); };
+   bool issamepos(int i,const Knode &n, double tol) const { return path[i].isSamePos(n,tol); };
    /* twnode related */
    bool checkintegrity(int i) const {return path[i].checkintegrity();};
    bool checkintegrity() const {  //of all nodes in path
@@ -45,6 +47,7 @@ template <class knode> class Twpath {
    bool hasnogoods(int i) const { return path[i].hasnogoods(); };
    bool earlyarrival(int i,const double D) const { return path[i].earlyarrival(D); };
    bool latearrival(int i,const double D) const { return path[i].latearrival(D); };
+   bool ontime(int i, const double D) const {return not earlyarrival(D) and not latearrival(D);};
 
 
     int opens(int i) const {return path[i].opens();};
@@ -56,9 +59,9 @@ template <class knode> class Twpath {
 
  
 /* path specific operations */
-    typedef typename std::deque<knode> nodepath;
-    typedef typename std::deque<knode>::iterator iterator;
-    typedef typename std::deque<knode>::const_iterator const_iterator;
+    typedef typename std::deque<Knode> nodepath;
+    typedef typename std::deque<Knode>::iterator iterator;
+    typedef typename std::deque<Knode>::const_iterator const_iterator;
 
     Twpath& operator=(const Twpath& n) {
         home = n.home;
@@ -66,27 +69,31 @@ template <class knode> class Twpath {
         return *this;
     };
 
-    knode& getdepot() { return home; };
-    knode& getdumpsite() { return dumpsite; };
+    Knode& getdepot() { return home; };
+    Knode& getdumpsite() { return dumpsite; };
 
-    void setdepot(knode& n) { home = n; };
-    void setdumpsite(knode& n) { dumpsite = n; };
+    void setdepot(Knode& n) { home = n; };
+    void setdumpsite(Knode& n) { dumpsite = n; };
 
     // element access
-    knode& operator[](unsigned int n) { return path[n]; };
-    knode& at(int n) { return path.at(n); };
-    knode& front() { return path.front(); };
-    knode& back() { return path.back(); };
+    Knode& operator[](unsigned int n) { return path[n]; };
+    Knode& at(int n) { return path.at(n); };
+    Knode& front() { return path.front(); };
+    Knode& back() { return path.back(); };
 
     // iterators
     iterator begin() { return path.begin(); };
     iterator end() { return path.end(); };
     iterator rbegin() { return path.rbegin(); };
-    //iterator rend() { return path.rend(); };
-    //const_iterator cbegin() { return path.cbegin(); };
-    //const_iterator cend() { return path.cend(); };
-    //const_iterator crbegin() { return path.crbegin(); };
-    //const_iterator crend() { return path.crend(); };
+    iterator rend() { return path.rend(); };
+    const_iterator cbegin() { return path.cbegin(); };
+    const_iterator cend() { return path.cend(); };
+    const_iterator crbegin() { return path.crbegin(); };
+    const_iterator crend() { return path.crend(); };
+
+
+    //operators
+//     Knode& operator[] (const int nIndex) {return path[nIndex];};
 
     // Capacity
     unsigned int size() const { return path.size(); };
@@ -96,16 +103,20 @@ template <class knode> class Twpath {
     //void shrink_to_fit() { path.shrink_to_fit(); };
 
     // modifiers
-    void push_back(const knode& n) { path.push_back(n); };
-    void push_back(knode& n) { path.push_back(n); };
-    void push_front(const knode& n) { path.push_front(n); };
-    void push_front(knode& n) { path.push_front(n); };
+    void push_back(const Knode& n) { path.push_back(n); };
+    void push_back(Knode& n) { path.push_back(n); };
+    void push_front(const Knode& n) { path.push_front(n); };
+    void push_front(Knode& n) { path.push_front(n); };
     void pop_back() { path.pop_back(); };
     void pop_front() { path.pop_front(); };
-    iterator insert(iterator it, const knode& n) { return path.insert(it, n); };
-    //iterator insert(iterator it, Trashnode& n) { return path.insert(it, n); };
-    //iterator erase(iterator it) { return path.erase(it); };
-    //iterator erase(iterator first, iterator last) { return path.erase(first, last); };
+    void insert (int at, const Knode &n) { path.insert(path.begin()+at,n);};
+    void insert (int at, Knode &n) { path.insert(path.begin()+at,n);};
+    void erase (int at) { path.erase(path.begin()+at);};
+
+    //iterator insert(iterator it, const Knode& n) { return path.insert(it, n); };
+    //iterator insert(iterator it, Knode& n) { return path.insert(it, n); };
+    iterator erase(iterator it) { return path.erase(it); };
+    iterator erase(iterator first, iterator last) { return path.erase(first, last); };
     void clear() { path.clear(); };
     //iterator emplace(const_iterator it, const Trashnode& n) { return path.emplace(it, n); };
     //iterator emplace_front(const Trashnode& n) { return path.emplace_front(n); };
