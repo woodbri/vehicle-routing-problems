7bde003857264e2edc8abdd1f2590762cd26423e
Stephen Woodbridge <woodbri@swoodbridge.com>
2014-11-04 15:22:34 -0500

    ifdef out call to compareCostWithOSRM in solverTrash/tabuopt.cpp

--- solverTrash/tabuopt.cpp
+++ solverTrash/tabuopt.cpp
@@ -478,7 +478,7 @@ std::cout<<"Entering TabuOpt::classifyMoves \n";
         OptSol current=currentSolution;
         std::sort(neighborhood.begin(), neighborhood.end(), Move::bySavings); 
 
-#ifndef VICKY
+#ifdef COMPARE_OSRM
         compareCostWithOSRM(neighborhood);
 #endif
 
