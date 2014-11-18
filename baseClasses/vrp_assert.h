1b187ba808c44ccaccb7b6f2aa4536b61f507b5c
Stephen Woodbridge <woodbri@swoodbridge.com>
2014-11-17 20:51:45 -0500

    Creating vrp_assert.h to convert failed assert() into throw()

--- baseClasses/vrp_assert.h
+++ baseClasses/vrp_assert.h
@@ -1,4 +1,12 @@
-
+/*! \file vrp_assert.h
+ * \brief An assert functionality that uses C++ throw().
+ *
+ * This file provides an alternative to assert functionality that will
+ * convert all assert() into C++ throw using an AssertFailedException class.
+ * This is allows us to catch these errors and do appropriate clean up and
+ * re-throw if needed so we can catch errors in the postgresql environment
+ * so we do not crash the backend server.
+ */
 #ifndef VRP_ASSERT_H
 #define VRP_ASSERT_H
 
@@ -8,17 +16,54 @@
 #undef assert
 #endif
 
+#ifndef __STRING
+#define __STRING(x) #x
+#endif
+
+#define __TOSTRING(x) __STRING(x)
+
+/*! \def assert(expr)
+ * \brief Uses the standard assert syntax.
+ *
+ * When an assertion fails it will throw \ref AssertFailedException and what()
+ * will return a string like "AssertFailedException(2+2 == 5) at t.cpp:11"
+ *
+ * Here is an example of using it:
+ * \code
+    #include <iostream>
+    #include "vrp_assert.h"
+
+    int main() {
+
+        try {
+            assert(2+2 == 4);
+            assert(2+2 == 5);
+        }
+        catch (AssertFailedException &e) {
+            std::cout << e.what() << "\n";
+        }
+        catch (std::exception& e) {
+            std::cout << e.what() << "\n";
+        }
+        catch(...) {
+            std::cout << "Caught unknown exception!\n";
+        }
+        return 0;
+    }
+\endcode
+ */
 #define assert(expr) \
     ((expr) \
      ? static_cast<void>(0) \
-     : __assert_failed( __STRING(expr), __FILE__,  __LINE__ ,  __PRETTY_FUNCTION__ ))
+     : throw AssertFailedException( "AssertFailedException: " __STRING(expr) " at " __FILE__ ":" __TOSTRING(__LINE__) ))
 
-#define __assert_failed(a, b, c, d) \
-    throw new AssertFailedException( "AssertFailedException: " #a ", " #b ", " #c ", " #d )
 
+/*! \class AssertFailedException
+ * \brief Extends std::exception and is the exception that we throw if an assert fails.
+ */
 class AssertFailedException : public std::exception {
   private:
-    const char * str;
+    const char * str;   ///< str Holds the what() string for the exception.
 
   public:
     virtual const char * what() const throw() {
