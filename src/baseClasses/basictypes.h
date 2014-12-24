ff0077b7e3fb0bf6fe52e72f97db6c1426aa42fe
mapas de vicky <vicky_vergara@hotmail.com>
2014-12-23 20:39:52 -0600

    missing files

---
+++ src/baseClasses/basictypes.h
@@ -0,0 +1,13 @@
+#ifndef SRC_BASECLASSES_BASICTYPES_H_
+#define SRC_BASECLASSES_BASICTYPES_H_
+
+#include <stdint.h>
+
+  typedef uint64_t POS;
+  typedef uint64_t UINT;
+  typedef uint64_t UID;
+
+  double VRP_MAX();
+  double VRP_MIN();
+
+#endif
